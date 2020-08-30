b24a2bb30ec14485859b89ae3496b5048adf233c
Adam Zimowski <mrazjava@yandex.com>
2020-08-30 07:51:11 +0200

    renamed author id filter

--- src/main/java/com/github/mrazjava/booklink/openlibrary/dataimport/AuthorIdFilter.java
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/dataimport/AuthorIdFilter.java
@@ -20,26 +20,26 @@ public class AuthorIdFilter {
      * One ID per line. Comments are allowed and start with a #. Empty lines
      * are allowed and ignored.
      */
-    public static final String FILENAME_ALLOWED_IDS = "author-ids.txt";
+    public static final String FILENAME_AUTHOR_FILTER = "author-id-filter.txt";
 
     private Set<String> allowedIds = new HashSet<>();
 
     void load(File workingDirectory) {
 
-        File allowedIdsFile = new File(workingDirectory.getAbsolutePath() + File.separator + FILENAME_ALLOWED_IDS);
-        if(allowedIdsFile.exists()) {
-            log.info("detected author IDs file: {}", allowedIdsFile.getAbsoluteFile());
+        File authorIdFilter = new File(workingDirectory.getAbsolutePath() + File.separator + FILENAME_AUTHOR_FILTER);
+        if(authorIdFilter.exists()) {
+            log.info("detected author ID filter file: {}", authorIdFilter.getAbsoluteFile());
             try {
-                LineIterator iterator = FileUtils.lineIterator(allowedIdsFile, "UTF-8");
+                LineIterator iterator = FileUtils.lineIterator(authorIdFilter, "UTF-8");
                 while(iterator.hasNext()) {
                     String line = iterator.next();
                     if(StringUtils.isNotBlank(line) && !line.startsWith("#")) {
                         allowedIds.add(line);
                     }
                 }
-                log.info("loaded {} allowed IDs:\n{}", allowedIds.size(), allowedIds);
+                log.info("loaded {} author filter IDs:\n{}", allowedIds.size(), allowedIds);
             } catch (IOException e) {
-                log.error("problem loading author id file: {}", e.getMessage());
+                log.error("problem loading author id filter file: {}", e.getMessage());
                 allowedIds.clear();
             }
         }
