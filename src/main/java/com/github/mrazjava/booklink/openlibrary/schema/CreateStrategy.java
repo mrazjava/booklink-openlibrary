3d7a8f933e9ad9d6c7e2517e4fd43743ea02110f
Adam Zimowski <mrazjava@yandex.com>
2020-09-23 08:12:36 +0200

    schema cleanup javadoc

--- src/main/java/com/github/mrazjava/booklink/openlibrary/schema/CreateStrategy.java
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/schema/CreateStrategy.java
@@ -1,8 +1,5 @@
 package com.github.mrazjava.booklink.openlibrary.schema;
 
-/**
- * @author Adam Zimowski (<a href="mailto:adam.zimowski@freenetdigital.com">azimowski</a>)
- */
 public enum CreateStrategy {
 
     unless_exists;
