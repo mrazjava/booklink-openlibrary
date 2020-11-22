032fc0b8ee773077be5ff8cfd0e62ba6de93cca6
Adam Zimowski <mrazjava@yandex.com>
2020-11-22 16:15:35 +0100

    search editions by works

--- src/main/java/com/github/mrazjava/booklink/openlibrary/depot/service/EditionService.java
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/depot/service/EditionService.java
@@ -3,12 +3,18 @@ package com.github.mrazjava.booklink.openlibrary.depot.service;
 import com.github.mrazjava.booklink.openlibrary.depot.DepotEdition;
 import com.github.mrazjava.booklink.openlibrary.repository.EditionRepository;
 import com.github.mrazjava.booklink.openlibrary.schema.EditionSchema;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Service
 public class EditionService extends AbstractDepotService<DepotEdition, EditionSchema> {
+
+    @Autowired
+    private EditionRepository editionRepository;
+
 
     @Override
     protected Function<EditionSchema, DepotEdition> schemaToDepot() {
@@ -28,5 +34,9 @@ public class EditionService extends AbstractDepotService<DepotEdition, EditionSc
     @Override
     protected String getCollectionName() {
         return EditionRepository.COLLECTION_NAME;
+    }
+
+    public List<EditionSchema> findByWorkId(String workId) {
+        return editionRepository.findByWorks(List.of(workId));
     }
 }
--- src/main/java/com/github/mrazjava/booklink/openlibrary/repository/EditionRepository.java
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/repository/EditionRepository.java
@@ -1,11 +1,12 @@
 package com.github.mrazjava.booklink.openlibrary.repository;
 
 import com.github.mrazjava.booklink.openlibrary.schema.EditionSchema;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface EditionRepository extends OpenLibraryMongoRepository<EditionSchema> {
 
     public static final String COLLECTION_NAME = "editions";
+
+    List<EditionSchema> findByWorks(List<String> workIds);
 }
