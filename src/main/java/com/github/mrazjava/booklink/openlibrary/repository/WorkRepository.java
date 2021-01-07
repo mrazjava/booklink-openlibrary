39da8eeb957ebe9d2a2a1d145f0a0a85dc259e24
Adam Zimowski <mrazjava@protonmail.com>
2021-01-07 09:35:30 +0100

    resolve unused import

--- src/main/java/com/github/mrazjava/booklink/openlibrary/repository/WorkRepository.java
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/repository/WorkRepository.java
@@ -1,7 +1,6 @@
 package com.github.mrazjava.booklink.openlibrary.repository;
 
 import com.github.mrazjava.booklink.openlibrary.schema.WorkSchema;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface WorkRepository extends OpenLibraryMongoRepository<WorkSchema> {
 
