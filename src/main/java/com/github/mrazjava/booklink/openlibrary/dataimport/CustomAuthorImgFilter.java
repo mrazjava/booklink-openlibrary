93ed45176f1a347b929b5fe1e99000e952701343
Adam Zimowski <mrazjava@protonmail.com>
2021-01-10 10:24:09 +0100

    custom author img filter
    Used to incorporate images other than those provided by openlibrary.org

---
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/dataimport/CustomAuthorImgFilter.java
@@ -0,0 +1,25 @@
+package com.github.mrazjava.booklink.openlibrary.dataimport;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthorImgFilter extends AbstractIdFilter {
+
+    public static final String FILTER_NAME = "WORK-ID";
+
+    /**
+     * Optional file; if exists, only works in that list will be persisted.
+     * One ID per line. Comments are allowed and start with a #. Empty lines
+     * are allowed and ignored.
+     */
+    public static final String FILENAME_WORK_FILTER = "custom-author-img-filter.txt";
+
+    public CustomAuthorImgFilter(String filterFilename) {
+        super(filterFilename);
+    }
+
+    @Override
+    protected String getFilterName() {
+        return FILENAME_WORK_FILTER;
+    }
+}
