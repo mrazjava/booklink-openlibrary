ca9f916a8d8cdf6518f59b9470c8ff5dd228a804
Adam Zimowski <mrazjava@protonmail.com>
2021-01-16 18:14:59 +0100

    plain cover filter for works

---
+++ src/main/java/com/github/mrazjava/booklink/openlibrary/dataimport/filter/PlainWorkCoverFilter.java
@@ -0,0 +1,26 @@
+package com.github.mrazjava.booklink.openlibrary.dataimport.filter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlainWorkCoverFilter extends AbstractIdFilter {
+
+    public static final String FILTER_NAME = "WORK-PLAIN-COVER-FILTER";
+
+    /**
+     * Optional file; if exists, covers matching IDs in this filter will be marked as 
+     * plain ({@link CoverImage#setPlain(Boolean)).
+     */
+    public static final String FILTER_FILENAME = "author-img-exclusions.txt";
+
+
+    PlainWorkCoverFilter(String filterFilename) {
+        super(FILTER_NAME);
+    }
+
+    @Override
+    public String getFilterName() {
+        return FILTER_NAME;
+    }
+
+}
